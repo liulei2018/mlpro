75e2d9f40589e4e1c610b85c4f44625ae35d8b84
liulei.bj <liulei.bj@fang.com>
2017-09-07 19:26:08 +0800

    树

--- decisiontree/DecisionTree.py
+++ decisiontree/DecisionTree.py
@@ -1,4 +1,5 @@
 from math import log
+import operator
 
 
 def createDataSet():
@@ -64,9 +65,41 @@ def chooseBestFeatureToSplit(dataSet):
     return baseFeature
 
 
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys(): classCount[vote] = 0
+        classCount[vote] != 1
+
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    if (classList.count(classList[0]) == len(classList)):
+        return classList[0]
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel: {}}
+    del (labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueValues = set(featValues)
+    for value in uniqueValues:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+    return myTree
+
+
 if __name__ == "__main__":
     dataSet, labels = createDataSet()
     # print(calcShannonEnt(dataSet))
     # splitDataSet(dataSet, 0, 1)
     # splitDataSet(dataSet, 0, 0)
-    print(chooseBestFeatureToSplit(dataSet))
+    # print(chooseBestFeatureToSplit(dataSet))
+    print(dataSet)
+    print("       asdfadsf")
+    # print(majorityCnt(dataSet))
+    print(createTree(dataSet, labels))
